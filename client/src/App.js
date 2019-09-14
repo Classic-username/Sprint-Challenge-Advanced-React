bb8c841e94516f1e06b0ae2b9b7403722036f921
Joshua Draper <josh.drapergame@gmail.com>
2019-09-13 20:24:03 -0600

    Class data extracted out of app, still works.

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Axios from 'axios';
-import PlayerCard from './components/PlayerCard';
+import PlayerData from './components/PlayerData';
 
-class App extends React.Component {
-
-  constructor() {
-    super();
-    this.state = {
-      players: []
-    }
-  }
-  
-
-  componentDidMount() {
-    const url = 'http://localhost:5000/api/players'
-    Axios
-      .get(url)
-      .then(res => this.setState({ players: res.data }))
-      .then(console.log(this.state, 'inside CDM'))
-      .catch(err => console.log(err,'error'))
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log(this.state, 'inside CDU')
-    // if (prevState.players !== this.state.players){
-    //   console.log(this.state.players,'in CompDidUp')
-
-    // }
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <PlayerCard players={this.state.players}/>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Advanced React Sprint: this displays the Women's World Cup most searched women.</h2>
+        <PlayerData />
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- client/src/components/PlayerCard.js
+++ client/src/components/PlayerCard.js
@@ -2,17 +2,14 @@ import React, { useEffect, useState } from 'react';
 
 const PlayerCard = (props) => {
 
-    console.log(props.players,'props PlayerCardComp')
-
     return(
-        
 
         <div>
             {props.players.map(player => (
                 <div key={player.id}>
-                    <h2>Name: {player.name}</h2>
+                    <h3>Name: {player.name}</h3>
                     <p>Country: {player.country}</p>
-                    <p>Searched for {player.searches} times.</p>
+                    <p>Searched for {player.searches} time(s).</p>
                 </div>
             ))}
             <h1>Hello!</h1>
