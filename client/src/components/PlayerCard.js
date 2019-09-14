71ac1172f5992b12351c0fd86e53986add5f0aef
Joshua Draper <josh.drapergame@gmail.com>
2019-09-13 20:07:30 -0600

    information renders correctly! WOOOOOO

--- client/src/components/PlayerCard.js
+++ client/src/components/PlayerCard.js
@@ -2,10 +2,19 @@ import React, { useEffect, useState } from 'react';
 
 const PlayerCard = (props) => {
 
-    console.log(props,'props PlayerCardComp')
+    console.log(props.players,'props PlayerCardComp')
 
     return(
+        
+
         <div>
+            {props.players.map(player => (
+                <div key={player.id}>
+                    <h2>Name: {player.name}</h2>
+                    <p>Country: {player.country}</p>
+                    <p>Searched for {player.searches} times.</p>
+                </div>
+            ))}
             <h1>Hello!</h1>
         </div>
     )
